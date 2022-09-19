12c1186d646349f759b7256e266efa9a6b2083c0
AndreyAnisimovTerricon <atriummebel79@yandex.ru>
2022-09-19 22:02:40 +0300

    Сумма элементов в промежутке от M до N

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+
+int SumRec (int n)
+{
+    if (n == m) return m;
+    else return n = n + SumRec(n-1);
+}
+Console.WriteLine(SumRec(n));
